7c27f1ec5dcaae10a9be69538a0e39ebcaf0846c
pjaeyoung <devdodose@gmail.com>
2021-08-05 01:22:01 +0900

    feat: AuthInput의 에러 상태일 때 스타일 적용

--- src/components/AuthInput/index.jsx
+++ src/components/AuthInput/index.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { Field } from 'formik'
 import styles from './index.module.css'
+import classNames from 'classnames'
 
-function AuthInput({ name, type, disabled, placeholder, validate, children }) {
+function AuthInput({
+  name,
+  existingError = false,
+  type,
+  disabled,
+  placeholder,
+  maxLength,
+  validate,
+  children,
+}) {
   return (
     <label className={styles.label}>
       {children}
       <Field
         name={name}
-        className={styles.input}
+        className={classNames(styles.input, {
+          [`${styles.error}`]: existingError,
+        })}
         type={type}
         placeholder={placeholder}
+        maxLength={maxLength}
         disabled={disabled}
         validate={validate}
       />
