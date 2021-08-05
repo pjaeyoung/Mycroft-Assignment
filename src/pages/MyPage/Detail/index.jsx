02cfe9e4eced8bedf011d550b4f78a16c21aaa47
pjaeyoung <devdodose@gmail.com>
2021-08-05 22:40:56 +0900

    chore: 사용하지 않는 임포트 삭제

--- src/pages/MyPage/Detail/index.jsx
+++ src/pages/MyPage/Detail/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FetchErrorMessage, OrderItem, Title } from '../../../components'
+import { FetchErrorMessage, Title } from '../../../components'
 
 import { useParams } from 'react-router-dom'
 
